b25d0e47008c8aeefa76c751142cadb03774b3b5
James Cunningham <james@bauscode.com>
2017-03-29 21:48:40 -0600

    chore(DERP) change to global error handling to include status info

--- src/configuration.js
+++ src/configuration.js
@@ -1,14 +1,17 @@
 //@flow
 
 class config {
-  token : string = '';
-  apiRoot : string = '';
-  throwErrors : bool = true;
-  globalErrorHandler = (e : Error) => {
-    console.error(e);
+  token: string = '';
+  apiRoot: string = '';
+  throwErrors: bool = true;
+  globalErrorHandler = (e: Error, statusCode: number, message: string) => {
+    console.error(e, statusCode, message);
+    if (this.throwErrors) {
+      throw e;
+    }
   };
 
-  getAuthorizationHeader() : string {
+  getAuthorizationHeader(): string {
     return `Bearer ${this.token}`;
   }
 };
