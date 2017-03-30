959d74004728992512849aeb1ebb1d70a6fd8243
James Cunningham <james@bauscode.com>
2017-03-29 22:10:55 -0600

    chore(DERP) simplify error handling

--- src/errorCodes.js
+++ src/errorCodes.js
@@ -5,35 +5,25 @@ import {
 import Configuration from './configuration';
 
 const knownCodes: {
-  '302': string,
-  '400': string,
-  '401': string,
-  '403': string,
-  '405': string,
-  '500': string,
-} = {
-  '302': 'Redirect',
-  '400': 'Validation',
-  '401': 'Unauthorized',
-  '403': 'Forbidden',
-  '405': 'Not Implemented',
-  '500': 'Server Error',
-};
+  [statusCode: number]: string
+} = {};
+
+knownCodes[302] = 'Redirect';
+knownCodes[400] = 'Validation';
+knownCodes[401] = 'Unauthorized';
+knownCodes[403] = 'Forbidden';
+knownCodes[405] = 'Not Implemented';
+knownCodes[500] = 'Server Error';
 
 /*
   Returns a function that handles the given error code.
   If there is no error, returns a NOOP-equivalent function.
  */
-export default function getErrorCodeHandler({
-  response,
-  json,
-}: Object): Function {
-  const {
-    status
-  } = response;
-  const handler = knownCodes[status];
-  if (handler) {
-    return () => Configuration.globalErrorHandler(new FetchError(json, handler), status, handler.message);
+export default function getErrorCodeHandler(response: Object): Function {
+  const { status } = response;
+  const message = knownCodes[status];
+  if (message) {
+    return () => Configuration.globalErrorHandler(new FetchError(response, message), status, message);
   }
 
   return () => {};
